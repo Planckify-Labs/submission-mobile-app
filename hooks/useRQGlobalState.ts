925ec4eaf71b2d511a4b77e9df62e6b976b4dc78
satriaali <satrialipatullah@gmail.com>
2025-05-24 07:17:16 +0700

    setup react query for client and server state management

---
+++ hooks/useRQGlobalState.ts
@@ -0,0 +1,36 @@
+import { queryClient } from "@/app/_layout";
+import {
+  type MutationFunction,
+  type QueryKey,
+  useMutation,
+  useQuery,
+} from "@tanstack/react-query";
+
+type TUseRQGlobalState<T> = {
+  initialData?: T;
+  queryKey: QueryKey;
+};
+
+export default function useRQGlobalState<T>({
+  initialData,
+  queryKey,
+}: TUseRQGlobalState<T>) {
+  const { data } = useQuery({
+    queryKey,
+    initialData: () => initialData,
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: async (newData) => {
+      return newData;
+    },
+    onSuccess: (newData) => {
+      queryClient.setQueryData(queryKey, newData);
+    },
+  });
+  const setNewData = (newData: T) => {
+    const executeMutation = mutate as MutationFunction<typeof newData, unknown>;
+    executeMutation(newData);
+  };
+  return { data, setNewData };
+}
