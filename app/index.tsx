05b891814b095e4f49b9caad3a77702aa191b385
satriaali <satrialipatullah@gmail.com>
2025-05-25 23:24:36 +0700

    feat: redirect user to login screen when there are no wallet detected - add loading spinner on loading wallet data

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
+import LoadinngSpinnerPopup from "@/components/common/LoadinngSpinnerPopup";
 import ActivitySection from "@/components/home/ActivitySection";
 import BalanceSection from "@/components/home/BalanceSection";
 import Header from "@/components/home/Header";
 import PaymentSection from "@/components/home/PaymentSection";
 import { useWallet } from "@/hooks/useWallet";
+import { router } from "expo-router";
 import { QrCode } from "lucide-react-native";
 import React from "react";
 import {
@@ -18,37 +20,53 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Home() {
   const { wallets, isLoading } = useWallet();
 
-  if (isLoading) {
-    return null;
-  }
+  React.useEffect(() => {
+    if (!isLoading && wallets.length === 0) {
+      const timeout = setTimeout(() => {
+        router.replace("/login");
+      }, 1500);
 
-  if (wallets.length === 0) {
-    return null;
+      return () => clearTimeout(timeout);
+    }
+  }, [isLoading, wallets.length]);
+
+  if (isLoading || wallets.length === 0) {
+    return (
+      <SafeAreaView style={styles.container} edges={["top"]}>
+        <LoadinngSpinnerPopup
+          visible={true}
+          title="Getting Ready"
+          message="Please wait while we making sure you land safely on TakumiPay..."
+        />
+      </SafeAreaView>
+    );
   }
 
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#f5f6f9" />
-      <SafeAreaView style={styles.container} edges={["top"]}>
-        <ScrollView
-          className="bg-light-main-container flex-1"
-          contentContainerStyle={{ gap: 16 }}
-          showsVerticalScrollIndicator={false}
-        >
-          <View className="flex-1 gap-4 p-4 pb-24">
-            <Header />
-            <BalanceSection />
-            <ActivitySection />
-            <PaymentSection />
+      {!isLoading && wallets.length !== 0 && (
+        <SafeAreaView style={styles.container} edges={["top"]}>
+          <ScrollView
+            className="bg-light-main-container flex-1"
+            contentContainerStyle={{ gap: 16 }}
+            showsVerticalScrollIndicator={false}
+          >
+            <View className="flex-1 gap-4 p-4 pb-24">
+              <Header />
+              <BalanceSection />
+              <ActivitySection />
+              <PaymentSection />
+            </View>
+          </ScrollView>
+          <View className="absolute bottom-2 justify-center items-center w-full">
+            <Pressable className="bg-light-primary-red px-10 py-4 rounded-full flex-row items-center gap-2">
+              <QrCode size={22} color="#fff" />
+              <Text className="text-light font-bold text-2xl">Scan To Pay</Text>
+            </Pressable>
           </View>
-        </ScrollView>
-        <View className="absolute bottom-2 justify-center items-center w-full">
-          <Pressable className="bg-light-primary-red px-10 py-4 rounded-full flex-row items-center gap-2">
-            <QrCode size={22} color="#fff" />
-            <Text className="text-light font-bold text-2xl">Scan To Pay</Text>
-          </Pressable>
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      )}
     </>
   );
 }
