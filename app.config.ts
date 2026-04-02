e950994acd882050266d8c1c65aa20542313fdc4
Satria Ali <satrialipatullah@gmail.com>
2026-04-03 06:43:23 +0700

    chore: bump app version to 1.1.0

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "takumiwallet",
-  version: "1.0.0",
+  version: "1.1.0",
   orientation: "portrait",
   icon: "./assets/images/takumipay-logo.png",
   scheme: getScheme(),
