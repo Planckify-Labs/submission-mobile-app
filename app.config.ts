315bad802975d081ea45288693d938f5baca4075
Satria Ali <satrialipatullah@gmail.com>
2026-04-05 15:25:00 +0700

    chore: bump version to 1.2.2 and temporarily disable promotion banner in service screen

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "takumiwallet",
-  version: "1.1.2",
+  version: "1.2.2",
   orientation: "portrait",
   icon: "./assets/images/takumipay-logo.png",
   scheme: getScheme(),
